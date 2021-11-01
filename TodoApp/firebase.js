b335d55620e6c469ee8fa96fa2d8f27905d89d0e
Sudeep Balagopal <60956438+Sudeep732000@users.noreply.github.com>
2021-11-01 15:02:28 +0530

    Update firebase.js

--- TodoApp/firebase.js
+++ TodoApp/firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyDJDdkeScer2VSyVhuO_s2J3Eju4dhV4j0",
-    authDomain: "todoapp-a32dc.firebaseapp.com",
-    projectId: "todoapp-a32dc",
-    storageBucket: "todoapp-a32dc.appspot.com",
-    messagingSenderId: "263288530680",
-    appId: "1:263288530680:web:3c4069cc105a0d001b3d0d",
-    measurementId: "G-1HXJ2EWQC8"
+    apiKey: "Your API key",
+    authDomain: "doamin id",
+    projectId: "id",
+    storageBucket: "storage permission",
+    messagingSenderId: "id",
+    appId: "id",
+    measurementId: "value id"
   });
 
   // Initialize Firebase
